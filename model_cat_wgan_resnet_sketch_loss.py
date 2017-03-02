01cdc31e7300131e2020c05ba5508b22dff3811d
Jerry Li <jerrylijiaming@gmail.com>
2017-03-03 09:04:04 +1100

    Trying sketch loss

--- model_cat_wgan_resnet_sketch_loss.py
+++ model_cat_wgan_resnet_sketch_loss.py
@@ -1,5 +1,6 @@
 import tensorflow as tf
 import tensorflow.contrib.slim as slim
+import numpy as np
 
 from inception_resnet_v1 import inference
 
@@ -7,13 +8,14 @@ from inception_resnet_v1 import inference
 class DTN(object):
     """Domain Transfer Network
     """
-    def __init__(self, mode='train', learning_rate=0.0003, num_classes = 10, hw = 128, alpha=15, beta=15):
+    def __init__(self, mode='train', learning_rate=0.0003, num_classes = 10, hw = 128, alpha=15, beta=15, gamma=15):
         self.mode = mode
         self.learning_rate = learning_rate
         self.num_classes = num_classes
         self.hw = hw
         self.alpha = alpha
         self.beta=beta
+        self.gamma = gamma # For sketch losses.
         assert hw==128
         
     def content_extractor(self, images, reuse=False):
@@ -57,9 +59,9 @@ class DTN(object):
                         net = slim.conv2d_transpose(net, 3, [3, 3], activation_fn=tf.nn.tanh, scope='conv_transpose4')   # (batch_size, 128, 128, 3)
                         return net
     
-    def discriminator(self, images, reuse=False):
+    def discriminator(self, images, var_scope = 'discriminator', reuse=False):
         # images: (batch, 32, 32, 3)
-        with tf.variable_scope('discriminator', reuse=reuse):
+        with tf.variable_scope(var_scope, reuse=reuse):
             with slim.arg_scope([slim.conv2d], padding='SAME', activation_fn=None,
                                  weights_initializer=tf.contrib.layers.xavier_initializer()):
                 with slim.arg_scope([slim.batch_norm], decay=0.95, center=True, scale=True, 
@@ -93,6 +95,29 @@ class DTN(object):
                     net = slim.flatten(net)
                     return net
 
+    def sketch_extractor(self, images):
+        # Must feed in images represented in range 0`255.
+        image_shape = images.get_shape().as_list()
+        if len(image_shape) != 4:
+            raise AssertionError("Input image must have shape [batch_size, height, width, 1 or 3]")
+        if image_shape[3] == 3:
+            gray_images = tf.image.rgb_to_grayscale(images)
+        elif image_shape[3] == 1:
+            gray_images = images
+        else:
+            raise AssertionError("Input image must have shape [batch_size, height, width, 1 or 3]")
+        filt = np.expand_dims(np.array([[1, 1, 1],
+                            [1, 1, 1],
+                            [1, 1, 1]],
+                            np.uint8),axis=2)
+        stride = 1
+        rate = 1
+        padding = 'SAME'
+        dil = tf.nn.dilation2d(gray_images, filt, (1,stride,stride,1), (1,rate,rate,1), padding, name='image_dilated')
+        sketch = 255 - tf.abs(gray_images - dil)
+        # Did NOT apply a threshold here to clear out the low values because i think it may not be necessary.
+        assert sketch.get_shape().as_list() == gray_images.get_shape().as_list()
+        return sketch
                 
     def build_model(self):
         
@@ -131,13 +156,15 @@ class DTN(object):
             self.fx = self.content_extractor(self.src_images)
             self.fake_images = self.generator(self.fx)
             self.logits = self.discriminator(self.fake_images)
+            self.fake_sketches = self.sketch_extractor(self.fake_images)
+            self.fake_sketches_logits = self.discriminator(self.fake_sketches, var_scope='discriminator_sketch')
             self.fgfx = self.content_extractor(self.fake_images, reuse=True)
 
             # loss
             # self.d_loss_src = slim.losses.sigmoid_cross_entropy(self.logits, tf.zeros_like(self.logits))
             # self.g_loss_src = slim.losses.sigmoid_cross_entropy(self.logits, tf.ones_like(self.logits))
-            self.d_loss_src = tf.reduce_mean(self.logits)
-            self.g_loss_src = - tf.reduce_mean(self.logits)
+            self.d_loss_src = tf.reduce_mean(self.logits) + tf.reduce_mean(self.fake_sketches_logits)
+            self.g_loss_src = - tf.reduce_mean(self.logits) - tf.reduce_mean(self.fake_sketches_logits)
             self.f_loss_src = tf.reduce_mean(tf.square(self.fx - self.fgfx)) * self.alpha
             
             # optimizer
@@ -146,6 +173,7 @@ class DTN(object):
             self.f_optimizer_src = tf.train.AdamOptimizer(self.learning_rate)
             
             t_vars = tf.trainable_variables()
+            # 'discriminator' should include vars for sketch discriminator as well.
             d_vars = [var for var in t_vars if 'discriminator' in var.name]
             g_vars = [var for var in t_vars if 'generator' in var.name]
             f_vars = [var for var in t_vars if 'InceptionResnetV1' in var.name]
@@ -166,25 +194,33 @@ class DTN(object):
             f_loss_src_summary = tf.summary.scalar('src_f_loss', self.f_loss_src)
             origin_images_summary = tf.summary.image('src_origin_images', self.src_images)
             sampled_images_summary = tf.summary.image('src_sampled_images', self.fake_images)
+            sampled_images_sketches_summary = tf.summary.image('src_sampled_images_sketches', self.fake_sketches)
             self.summary_op_src = tf.summary.merge([d_loss_src_summary, g_loss_src_summary, 
                                                     f_loss_src_summary, origin_images_summary, 
-                                                    sampled_images_summary])
+                                                    sampled_images_summary, sampled_images_sketches_summary])
             
             # target domain (mnist)
             self.fx = self.content_extractor(self.trg_images, reuse=True)
             self.reconst_images = self.generator(self.fx, reuse=True)
             self.logits_fake = self.discriminator(self.reconst_images, reuse=True)
             self.logits_real = self.discriminator(self.trg_images, reuse=True)
+            self.reconst_images_sketches = self.sketch_extractor(self.reconst_images)
+            self.reconst_images_sketches_logits = self.discriminator(self.reconst_images_sketches, var_scope='discriminator_sketch', reuse=True)
+            self.trg_images_sketches = self.sketch_extractor(self.trg_images)
+            self.trg_images_sketches_logits = self.discriminator(self.trg_images_sketches, var_scope='discriminator_sketch', reuse=True)
             
             # loss
             # self.d_loss_fake_trg = slim.losses.sigmoid_cross_entropy(self.logits_fake, tf.zeros_like(self.logits_fake))
             # self.d_loss_real_trg = slim.losses.sigmoid_cross_entropy(self.logits_real, tf.ones_like(self.logits_real))
             self.d_loss_fake_trg = tf.reduce_mean(self.logits_fake)
             self.d_loss_real_trg = - tf.reduce_mean(self.logits_real)
-            self.d_loss_trg = self.d_loss_fake_trg + self.d_loss_real_trg
+            self.d_loss_fake_trg_sketch = tf.reduce_mean(self.reconst_images_sketches_logits)
+            self.d_loss_real_trg_sketch = - tf.reduce_mean(self.trg_images_sketches_logits)
+            self.d_loss_trg = self.d_loss_fake_trg + self.d_loss_real_trg + self.d_loss_fake_trg_sketch + self.d_loss_real_trg_sketch
             self.g_loss_fake_trg = - tf.reduce_mean(self.logits_fake)
             self.g_loss_const_trg = tf.reduce_mean(tf.square(self.trg_images - self.reconst_images)) * self.beta
-            self.g_loss_trg = self.g_loss_fake_trg + self.g_loss_const_trg
+            self.g_loss_const_trg_sketch = tf.reduce_mean(tf.square(self.trg_images_sketches - self.reconst_images_sketches)) * self.gamma
+            self.g_loss_trg = self.g_loss_fake_trg + self.g_loss_const_trg + self.g_loss_const_trg_sketch
             
             # optimizer
             self.d_optimizer_trg = tf.train.AdamOptimizer(self.learning_rate)
@@ -199,16 +235,24 @@ class DTN(object):
             # summary op
             d_loss_fake_trg_summary = tf.summary.scalar('trg_d_loss_fake', self.d_loss_fake_trg)
             d_loss_real_trg_summary = tf.summary.scalar('trg_d_loss_real', self.d_loss_real_trg)
+            d_loss_fake_trg_sketch_summary = tf.summary.scalar('trg_d_loss_fake_sketch', self.d_loss_fake_trg_sketch)
+            d_loss_real_trg_sketch_summary = tf.summary.scalar('trg_d_loss_real_sketch', self.d_loss_real_trg_sketch)
             d_loss_trg_summary = tf.summary.scalar('trg_d_loss', self.d_loss_trg)
             g_loss_fake_trg_summary = tf.summary.scalar('trg_g_loss_fake', self.g_loss_fake_trg)
             g_loss_const_trg_summary = tf.summary.scalar('trg_g_loss_const', self.g_loss_const_trg)
+            g_loss_const_trg_sketch_summary = tf.summary.scalar('trg_g_loss_const_sketch', self.g_loss_const_trg_sketch)
             g_loss_trg_summary = tf.summary.scalar('trg_g_loss', self.g_loss_trg)
             origin_images_summary = tf.summary.image('trg_origin_images', self.trg_images)
             sampled_images_summary = tf.summary.image('trg_reconstructed_images', self.reconst_images)
-            self.summary_op_trg = tf.summary.merge([d_loss_trg_summary, g_loss_trg_summary, 
+            origin_images_sketch_summary = tf.summary.image('trg_origin_images_sketch', self.trg_images_sketches)
+            sampled_images_sketch_summary = tf.summary.image('trg_reconstructed_images_sketch', self.reconst_images_sketches)
+            self.summary_op_trg = tf.summary.merge([d_loss_trg_summary, g_loss_trg_summary,
                                                     d_loss_fake_trg_summary, d_loss_real_trg_summary,
+                                                    d_loss_fake_trg_sketch_summary, d_loss_real_trg_sketch_summary,
                                                     g_loss_fake_trg_summary, g_loss_const_trg_summary,
-                                                    origin_images_summary, sampled_images_summary])
+                                                    g_loss_const_trg_sketch_summary,
+                                                    origin_images_summary, sampled_images_summary,
+                                                    origin_images_sketch_summary, sampled_images_sketch_summary])
             for var in tf.trainable_variables():
                 tf.summary.histogram(var.op.name, var)
             
---
+++ sketch_test.py
@@ -0,0 +1,64 @@
+import cv2
+from PIL import Image
+from model_cat_wgan_resnet_sketch_loss import *
+
+def _imread(path, shape=None, bw=False, rgba=False, dtype=np.float32):
+    # type: (str, tuple, bool, bool) -> np.ndarray
+    """
+
+    :param path: path to the image
+    :param shape: (Height, width)
+    :param bw: Whether the image is black and white.
+    :param rgba: Whether the image is in rgba format.
+    :return: np array with shape (height, width, num_color(1, 3, or 4))
+    """
+    assert not (bw and rgba)
+    if bw:
+        convert_format = 'L'
+    elif rgba:
+        convert_format = 'RGBA'
+    else:
+        convert_format = 'RGB'
+
+    if shape is None:
+        return np.asarray(Image.open(path).convert(convert_format), dtype)
+    else:
+        return np.asarray(Image.open(path).convert(convert_format).resize((shape[1], shape[0])), dtype)
+
+class SketchTest(tf.test.TestCase):
+    def test_sketch_generation(self):
+        with self.test_session() as sess:
+            batch_size = 1
+            height = 300
+            width = 300
+            num_features = 3
+
+            input_layer = tf.placeholder(dtype=tf.float32, shape=(batch_size, height, width, num_features))
+            dtn = DTN()
+            input_layer_processed = input_layer # / 127.5 - 1
+            sketch = dtn.sketch_extractor(input_layer_processed)
+            sketch_unprocessed = sketch# (sketch + 1) * 127.5
+
+
+            image_shape = input_layer.get_shape().as_list()
+            final_shape = sketch.get_shape().as_list()
+
+            self.assertAllEqual(image_shape[:-1], final_shape[:-1])
+            self.assertEqual(1, final_shape[-1])
+
+            sess.run(tf.initialize_all_variables())
+
+            input_image_path = "/home/xor/datasets/download/Andrea_Anders/0d358f8e6862151c5b06aec9d3530cdf08e450cc.jpg"
+            feed_input = np.expand_dims(_imread(input_image_path),axis=0)
+
+            feed_dict = {input_layer:feed_input}
+            actual_output = sketch_unprocessed.eval(feed_dict)
+            self.assertTrue(actual_output is not None, 'The unet failed to produce an output.')
+
+            cv2.imshow('Input', cv2.cvtColor(feed_input[0], cv2.COLOR_RGB2BGR).astype(np.uint8))
+            cv2.imshow('Sketch', actual_output[0].astype(np.uint8))
+            cv2.waitKey(0)
+
+
+if __name__ == '__main__':
+    tf.test.main()
